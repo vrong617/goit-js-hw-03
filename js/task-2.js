5e0bd9a2e8178743a4829b556580f25c4eaa388d
Roma Yanush <vrong617@gmail.com>
2024-10-15 10:34:19 +0300

    Create task-2.js

---
+++ js/task-2.js
@@ -0,0 +1,16 @@
+function makeArray(firstArray, secondArray, maxLength){
+    let resArray = firstArray.concat(secondArray);
+
+    if (resArray.length <= maxLength) {
+        return resArray;
+    } else {
+        return resArray.slice(0, maxLength);
+    }
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
