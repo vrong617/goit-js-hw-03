00688e40ddae47644a68195b197460537cc83fe2
Roma Yanush <vrong617@gmail.com>
2024-10-15 10:36:10 +0300

    Create task-3.js

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function filterArray(numbers, value){
+    let resArray = [];
+    for(const number of numbers){
+        if (number>value) resArray.push(number);
+    }
+
+    return resArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
